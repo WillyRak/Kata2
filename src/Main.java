650c5099c7975f97694357fae40f90ad200221d3
Willy <thewillyeb@gmail.com>
2023-12-23 15:01:05 +0000

    Finished Histogram Calculator class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import Kata2.Person;
 import Kata2.TsvFilePersonLoader;
+import Kata2.WeightHistogramCalculator;
 
 import java.util.List;
+import java.util.Map;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         List<Person> people = TsvFilePersonLoader.with("hw_25000.tsv").load();
-        for(Person person : people) {
-            System.out.println(person);
+        Map<String, Integer> histogram = new WeightHistogramCalculator(people).calculate();
+        for(String key : histogram.keySet().stream().sorted().toList()) {
+            System.out.println(key + " " + histogram.get(key));
         }
     }
 }
