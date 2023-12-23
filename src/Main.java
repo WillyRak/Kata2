00f0b4d4c91a77e1f6be05c4877a6dd4250c0a8f
Willy <thewillyeb@gmail.com>
2023-12-23 14:39:44 +0000

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import Kata2.Person;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person(1, 120, 120);
-        System.out.println(person);
+        System.out.println("Holaa");
     }
 }
