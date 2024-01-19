26405656e606606a68be980ad22a80b6b1567707
Willy <thewillyeb@gmail.com>
2024-01-19 13:49:39 +0000

    a

--- src/Kata2/HistogramCalculator.java
+++
@@ -1,7 +0,0 @@
-package Kata2;
-
-import java.util.Map;
-
-public interface HistogramCalculator {
-    Map<String, Integer> calculate();
-}
--- src/Kata2/PersonLoader.java
+++
@@ -1,7 +0,0 @@
-package Kata2;
-
-import java.util.List;
-
-public interface PersonLoader {
-    List<Person> load();
-}
