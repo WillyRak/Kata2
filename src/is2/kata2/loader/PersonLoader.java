1e9d4357202bfe9eb9902d3a7a5181c3f05bdf35
Willy <thewillyeb@gmail.com>
2024-01-19 15:14:40 +0000

    Finished FileLoader class

---
+++ src/is2/kata2/loader/PersonLoader.java
@@ -0,0 +1,9 @@
+package is2.kata2.loader;
+
+import is2.kata2.person.Person;
+
+import java.util.List;
+
+public interface PersonLoader {
+    List<Person> load();
+}
--- src/is2/kata2/main/Main.java
+++ src/is2/kata2/main/Main.java
@@ -1,12 +1,19 @@
 package is2.kata2.main;
 
+import is2.kata2.loader.TsvFilePersonLoader;
 import is2.kata2.person.Person;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Person(1, 60, 120);
-        System.out.println(person1);
+
+        List<Person> people = TsvFilePersonLoader.with("hw_25000.tsv").load();
+        for(Person p : people) {
+            System.out.println(p);
+        }
+
     }
 
 }
