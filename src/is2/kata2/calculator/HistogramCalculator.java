52839386fd42792f2c69b35bff7393f5fcb97e67
Willy <thewillyeb@gmail.com>
2024-01-19 15:22:16 +0000

    Finished calculator for weight histogram

---
+++ src/is2/kata2/calculator/HistogramCalculator.java
@@ -0,0 +1,7 @@
+package is2.kata2.calculator;
+
+import java.util.Map;
+
+public interface HistogramCalculator {
+    Map<String, Integer> calculate();
+}
--- src/is2/kata2/main/Main.java
+++ src/is2/kata2/main/Main.java
@@ -1,9 +1,11 @@
 package is2.kata2.main;
 
+import is2.kata2.calculator.WeightHistogramCalculator;
 import is2.kata2.loader.TsvFilePersonLoader;
 import is2.kata2.person.Person;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +14,11 @@ public class Main {
         List<Person> people = TsvFilePersonLoader.with("hw_25000.tsv").load();
         for(Person p : people) {
             System.out.println(p);
+        }
+
+        Map<String, Integer> histogram = new WeightHistogramCalculator(people).calculate();
+        for(String key : histogram.keySet().stream().sorted().toList()) {
+            System.out.println(key + " " + histogram.get(key));
         }
 
     }
