e8fc64c3de5f5498e45bc24f48b00726f2499a43
Willy <thewillyeb@gmail.com>
2024-01-19 15:06:12 +0000

    Finished Person class

---
+++ src/is2/kata2/main/Main.java
@@ -0,0 +1,12 @@
+package is2.kata2.main;
+
+import is2.kata2.person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person(1, 60, 120);
+        System.out.println(person1);
+    }
+
+}
